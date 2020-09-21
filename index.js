3bc42f1bdd63c0c97a67e24791c7d4aa96ba6080
Don Smith <hi@dons.dev>
2020-09-21 13:55:54 +1200

    fix recommended config

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
 module.exports = {
-  env: [
-    "browser",
-    "node",
-    "es2020",
-    "jest"
-  ],
-  plugins: [
-    "import",
-    "jest",
-    "node",
-    "promise",
-    "react",
-    "standard"
-  ],
-  extends: [
-    "standard",
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/react",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
-    "plugin:react/recommended",
-    "plugin:promise/recommended"
-  ],
-  rules: {
-    "eol-last": [
-     "error",
-     "always"
-    ],
-    "no-multiple-empty-lines": [
-     "error",
-     {
-      "max": 1,
-      "maxEOF": 0,
-      "maxBOF": 0
-     }
-    ],
-    "object-curly-spacing": [2, "always"],
-    "react/prop-types": 0
+  configs: {
+    recommended: {
+      env: [
+        "browser",
+        "node",
+        "es2020",
+        "jest"
+      ],
+      plugins: [
+        "import",
+        "jest",
+        "node",
+        "promise",
+        "react",
+        "standard"
+      ],
+      extends: [
+        "standard",
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/react",
+        "plugin:jest/recommended",
+        "plugin:jest/style",
+        "plugin:react/recommended",
+        "plugin:promise/recommended"
+      ],
+      rules: {
+        "eol-last": [
+         "error",
+         "always"
+        ],
+        "no-multiple-empty-lines": [
+         "error",
+         {
+          "max": 1,
+          "maxEOF": 0,
+          "maxBOF": 0
+         }
+        ],
+        "object-curly-spacing": [2, "always"],
+        "react/prop-types": 0
+      }
+    }
   }
 }
