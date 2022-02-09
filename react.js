8cbc57188d0a423b330b3afbe1730e042feaa188
Gerard Paapu <gerard@gerardpaapu.com>
2022-02-09 13:07:48 +1300

    Add recommended react rules

--- react.js
+++ react.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   env: defaults.env,
   plugins: [...defaults.plugins, 'react'],
-  extends: [...defaults.extends, 'prettier/react'],
+  extends: [...defaults.extends, 'react/recommended', 'prettier/react'],
   settings: {
     ...defaults.settings,
     react: { version: 'detect' },
