81cfeb53f0d9bf2a198cf19962b29dd1cc97dae8
Gerard Paapu <gerard@gerardpaapu.com>
2022-02-10 11:05:31 +1300

    Split core and fix dependencies
    According to the eslint documentation for custom configs, parsers and
    configs you depend on belong in the `dependencies` and plugins belong
    in the `peerDependencies`. This means that for npm users before v7 they
    must manually install our peerDependencies. I'll this to the
    documentation (cribbed from the docs for the airbnb config)
    
    We now provide three configs:
    - `eda/core` the smallest set
    - `eda` core + jest
    - `eda/react` core + jest + react
    
    This covers all our existing challenges, in the future we might need one
    that provides react but not jest ...

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
+const defaults = require('./core')
+
 module.exports = {
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    sourceType: 'module',
-    requireConfigFile: false,
-  },
-  env: {
-    browser: true,
-    node: true,
-    es2020: true,
-    jest: true,
-  },
-  plugins: ['import', 'jest', 'node', 'prettier', 'promise'],
+  ...defaults,
+  env: { ...defaults.env, jest: true },
+  plugins: [...defaults.plugins, 'jest'],
   extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/react',
+    ...defaults.extends,
     'plugin:jest/recommended',
     'plugin:jest/style',
-    'plugin:promise/recommended',
-    'plugin:prettier/recommended',
   ],
-  settings: {
-    'import/resolver': { node: { extensions: ['.js'] } },
-  },
-  rules: {},
 }
