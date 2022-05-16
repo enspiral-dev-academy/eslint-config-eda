72dc2e4209192c977f2e48f9b63c49eb852a4923
Elise Wei <elise.wei@gmail.com>
2022-05-16 13:25:52 +1200

    Cleanup some redundant stuff

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const defaults = require('./core')
 
 module.exports = {
   ...defaults,
-  env: { ...defaults.env, jest: true },
-  plugins: [...defaults.plugins, 'jest'],
   extends: [
     ...defaults.extends,
     'plugin:jest/recommended',
